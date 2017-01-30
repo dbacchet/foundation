c8ab3f31949196899c671cd4e10621d979f591ed
Davide Bacchet <davide.bacchet@nextev.com>
2017-01-29 17:55:35 -0800

    adding a dummy test file

---
+++ testing/test_pippo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <thread>
+
+#include "gtest/gtest.h"
+
+#include "common/foundation_types.h"
+
+
+TEST(Pippo, Creation) {
+    int pippo = 0;
+    ASSERT_EQ(pippo,0);
+    ASSERT_TRUE(true);
+}
+
+
